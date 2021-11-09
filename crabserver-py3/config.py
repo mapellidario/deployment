db1ab881400d109fbab42a4ca83dd2c752cdd7f1
Dario Mapelli <mapelli.dario@gmail.com>
2021-11-09 17:10:49 +0100

    crabserver-py3 - fixes?

--- crabserver-py3/config.py
+++ crabserver-py3/config.py
@@ -9,14 +9,14 @@ conf = Configuration()
 main = conf.section_('main')
 srv = main.section_('server')
 srv.thread_pool = 25
-main.application = 'crabserver'
+main.application = 'crabserver-py3'
 main.port = 8270
 main.index = 'data'
 
 main.authz_defaults = { 'role': None, 'group': None, 'site': None }
 main.section_('tools').section_('cms_auth').key_file = "%s/auth/wmcore-auth/header-auth-key" % __file__.rsplit('/', 3)[0]
 
-app = conf.section_('crabserver')
+app = conf.section_('crabserver-py3')
 app.admin = 'cms-service-webtools@cern.ch'
 app.description = 'CRABServer RESTFull API'
 app.title = 'CRABRESTFull'
@@ -30,9 +30,9 @@ data.object = 'CRABInterface.RESTBaseAPI.RESTBaseAPI'
 data.phedexurl = 'https://cmsweb.cern.ch/phedex/datasvc/xml/prod/'
 data.dbsurl = 'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
 data.defaultBlacklist = ['T0_CH_CERN']
-data.serverhostcert = "%s/auth/crabserver/dmwm-service-cert.pem" % __file__.rsplit('/', 3)[0]
-data.serverhostkey = "%s/auth/crabserver/dmwm-service-key.pem" % __file__.rsplit('/', 3)[0]
-data.credpath = '%s/state/crabserver/proxy/' % __file__.rsplit('/', 4)[0]
+data.serverhostcert = "%s/auth/crabserver-py3/dmwm-service-cert.pem" % __file__.rsplit('/', 3)[0]
+data.serverhostkey = "%s/auth/crabserver-py3/dmwm-service-key.pem" % __file__.rsplit('/', 3)[0]
+data.credpath = '%s/state/crabserver-py3/proxy/' % __file__.rsplit('/', 4)[0]
 data.db = 'CRABServerAuth.dbconfig'
 data.backend = 'oracle'
 data.workflowManager = 'HTCondorDataWorkflow'
@@ -40,4 +40,4 @@ data.workflowManager = 'HTCondorDataWorkflow'
 data.extconfigurl = 'http://gitlab.cern.ch/crab3/CRAB3ServerConfig/raw/master/cmsweb-rest-config.json'
 
 data.loggingLevel = 10
-data.loggingFile = '%s/logs/crabserver/CRAB-%s.log' % (__file__.rsplit('/', 4)[0], time.strftime("%Y%m%d"))
+data.loggingFile = '%s/logs/crabserver-py3/CRAB-%s.log' % (__file__.rsplit('/', 4)[0], time.strftime("%Y%m%d"))
